cc11aa5de535cc11ab108e143eb4c93c16c0ee00
tyabus <tyabustest@gmail.com>
2019-04-07 17:32:51 +0300

    Critical Fix: DON'T CHANGE WEAPON LIST

--- dlls/gauss.cpp
+++ dlls/gauss.cpp
@@ -43,7 +43,7 @@ enum gauss_e
 };
 
 LINK_ENTITY_TO_CLASS( weapon_gauss, CGauss )
-LINK_ENTITY_TO_CLASS( weapon_egon, CGauss )
+//LINK_ENTITY_TO_CLASS( weapon_egon, CGauss )
 
 float CGauss::GetFullChargeTime( void )
 {
